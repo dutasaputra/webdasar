8416a243017232285b8f586317258565a657de08
dutasaputra <i.20048@mhs.unesa.ac.id>
2023-04-10 16:41:47 +0800

    tambah file index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php 
+
+    // type data string
+    $nama_kamu = "Budi";
+    // type data int
+    $umur = 5;
+    // tanda petik dua
+    echo "Hello World, Welcome ".$nama_kamu;
+    echo "<br/>";
+    echo " Umur kamu ".$umur;
+
+    echo "<br/>";
+    echo "<br/>";
+
+    echo "Hello World, Welcome ".$nama_kamu." Umur kamu ".$umur." tahun";
+
+    echo "<br/>";
+    echo "<br/>";
+
+    echo "Berarti $nama_kamu lahir di tahun ";
+    echo 2023-$umur;
+
+    echo "<br/>";
+    echo "<br/>";
+
+    // tanda petik satu
+    echo 'Hello World';
+
+?>
