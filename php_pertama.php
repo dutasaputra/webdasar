d0a9f90cb7f86acfcda5ffadcedb31382d092766
dutasaputra <i.20048@mhs.unesa.ac.id>
2023-04-13 17:10:28 +0800

    latihan php 13 april

--- php_pertama.php
+++ php_pertama.php
@@ -1,3 +1,17 @@
-<?php 
+<!DOCTYPE html>
+<html lang="en">
 
-echo "Hello saya Duta";
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php for ($i=0; $i<100; $i++) { ?>
+    <h2>Hello Aku Duta</h2>
+    <?php } ?>
+</body>
+
+</html
